6ed45e531df566daa96614760bf12a212a93d3c5
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-02-12 15:22:11 -0300

    adicionado mais inimigos e ondas de inimigos

--- Type Survivor/Assets/Scripts/EnemySpawner.cs
+++ Type Survivor/Assets/Scripts/EnemySpawner.cs
@@ -17,24 +17,53 @@ public class EnemySpawner : MonoBehaviour
     private float dspawnDistance;
     private List<GameObject> spawnedEnemies = new List<GameObject>();
 
+    public List<WaveInfo> waves;
+    private int currentWave;
+    private float waveCounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        spawnCounter = timeToSpawn;
+        //spawnCounter = timeToSpawn;
         target = PlayerHealthController.instance.transform;
         dspawnDistance = Vector3.Distance(transform.position, maxSpawn.position) + 5f;
+
+        currentWave = -1;
+        GoToNextWave();
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnCounter -= Time.deltaTime;
+        /*spawnCounter -= Time.deltaTime;
         if(spawnCounter <= 0 )
         {
             spawnCounter = timeToSpawn;
 
             GameObject newEnemy = Instantiate(enemyToSpawn, SelectSpawnPoint() , transform.rotation);
             spawnedEnemies.Add(newEnemy);
+        }*/
+
+        if (PlayerHealthController.instance.gameObject.activeSelf)
+        {
+            if(currentWave <waves.Count)
+            {
+                waveCounter -= Time.deltaTime;
+                if(waveCounter <= 0)
+                {
+                    GoToNextWave();
+                }
+
+                spawnCounter -= Time.deltaTime;
+                if(spawnCounter <= 0)
+                {
+                    spawnCounter = waves[currentWave].timeBetweenSpawns;
+
+                    GameObject newEnemy = Instantiate(waves[currentWave].enemyToSpawn, SelectSpawnPoint(), Quaternion.identity);
+
+                    spawnedEnemies.Add(newEnemy);
+                }
+            }
         }
 
         transform.position = target.position;
@@ -109,4 +138,25 @@ public class EnemySpawner : MonoBehaviour
 
         return spawnPoint;
     }
+
+    public void GoToNextWave()
+    {
+        currentWave++;
+
+        if(currentWave >= waves.Count)
+        {
+            currentWave = waves.Count - 1;
+        }
+
+        waveCounter = waves[currentWave].waveLength;
+        spawnCounter = waves[currentWave].timeBetweenSpawns;
+    }
+}
+
+[System.Serializable]
+public class WaveInfo
+{
+    public GameObject enemyToSpawn;
+    public float waveLength = 10f;
+    public float timeBetweenSpawns = 1f;
 }
