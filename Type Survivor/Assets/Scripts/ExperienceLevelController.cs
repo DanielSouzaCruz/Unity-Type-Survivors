2aee753de1e1b83f10f4d2a4a051c403e5792e57
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-02-14 15:12:53 -0300

    experiencia

---
+++ Type Survivor/Assets/Scripts/ExperienceLevelController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceLevelController : MonoBehaviour
+{
+    public static ExperienceLevelController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int currentExperience;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetExp(int amountToGet)
+    {
+        currentExperience = amountToGet;
+    }
+}
