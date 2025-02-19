7fab1300069aec01c644e7bfe3390b576f849879
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-02-19 14:19:56 -0300

    Nivel da arma

--- Type Survivor/Assets/Scripts/Weapons/SpinWeapon.cs
+++ Type Survivor/Assets/Scripts/Weapons/SpinWeapon.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinWeapon : MonoBehaviour
+public class SpinWeapon : Weapon
 {
     public float rotateSpeed;
     public Transform holder, fireballToSpawn;
+    public EnemyDamager damager;
 
     public float timeBetweenSpawn;
     private float spawnCounter;
 
     void Start()
     {
-        
+        SetStats();
     }
 
     // Update is called once per frame
     void Update()
     {
-        holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime));
+        //holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime));
+        holder.rotation = Quaternion.Euler(0f, 0f, holder.rotation.eulerAngles.z + (rotateSpeed * Time.deltaTime * stats[weaponLevel].speed));
 
         spawnCounter -= Time.deltaTime;
         if(spawnCounter <= 0)
@@ -27,5 +29,25 @@ public class SpinWeapon : MonoBehaviour
 
             Instantiate(fireballToSpawn, fireballToSpawn.position, fireballToSpawn.rotation, holder).gameObject.SetActive(true);
         }
+
+        if(statsUpdated == true)
+        {
+            statsUpdated = false;
+
+            SetStats();
+        }
+    }
+
+    public void SetStats()
+    {
+        damager.damageAmount = stats[weaponLevel].damage;
+
+        transform.localScale = Vector3.one * stats[weaponLevel].range;
+
+        timeBetweenSpawn = stats[weaponLevel].timeBetweenAttacks;
+
+        damager.lifeTime = stats[weaponLevel].duration;
+
+        spawnCounter = 0f;
     }
 }
