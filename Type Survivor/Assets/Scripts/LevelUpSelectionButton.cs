abc0e00c50ea77d281b2134bd62c5e4438ed6eed
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-02-20 15:19:27 -0300

    setando a tela de upgrade final

--- Type Survivor/Assets/Scripts/LevelUpSelectionButton.cs
+++ Type Survivor/Assets/Scripts/LevelUpSelectionButton.cs
@@ -8,12 +8,27 @@ public class LevelUpSelectionButton : MonoBehaviour
 {
     public TMP_Text upgradeDescriptionText, nameLevelText;
     public Image weaponIcon;
+
+    private Weapon assignedWeapon;
    
     public void UpdateButtonDisplay(Weapon theWeapon)
     {
         upgradeDescriptionText.text = theWeapon.stats[theWeapon.weaponLevel].upgradeText;
         weaponIcon.sprite = theWeapon.icon;
 
-        nameLevelText.text = theWeapon.name + " - Lvl" + theWeapon.weaponLevel;
+        nameLevelText.text = theWeapon.name + " - Lvl " + theWeapon.weaponLevel;
+
+        assignedWeapon = theWeapon;
+    }
+
+    public void SelectUpgrade()
+    {
+        if(assignedWeapon != null)
+        {
+            assignedWeapon.LevelUp();
+
+            UiController.instance.levelUpPanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
     }
 }
