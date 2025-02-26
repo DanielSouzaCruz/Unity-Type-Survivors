96c1c83bf0ae278c92c3b4515ee0d9c6d8155ab1
DanielSouzaCruz <danielrenovar165@gmail.com>
2025-02-26 15:31:34 -0300

    Menu de upgrade de personagem

---
+++ Type Survivor/Assets/Scripts/PlayerStatUpgradeDisplay.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerStatUpgradeDisplay : MonoBehaviour
+{
+    public TMP_Text valueText, costText;
+
+    public GameObject upgradeButton;
+
+    
+}
